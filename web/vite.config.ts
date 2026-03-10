1522ab6bcb56188b42b08d9ad9a34cb265562217
geekjy <admin@9170.org>
2026-03-10 13:43:16 +0800

    实际运行测试

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    // 监听所有 IPv4 地址，允许局域网内其他设备访问
+    host: '0.0.0.0',
+    // 可选：指定端口，默认是 5173
+    port: 5173 
+  }
 })
